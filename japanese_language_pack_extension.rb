b2ac3e8db8ba806792940d2ee0a6bb7b04e993db
Jiro <jirogithub@tg.org>
2010-12-17 11:42:26 -0800

    updated url from jirogit to radiant

--- japanese_language_pack_extension.rb
+++ japanese_language_pack_extension.rb
@@ -1,7 +1,7 @@
 class JapaneseLanguagePackExtension < Radiant::Extension
   version "1.0.2"
   description "Provides Japanese translation for the Radiant admin interface"
-  url "http://github.com/jirogit/radiant-japanese_language_pack-extension"
+  url "http://github.com/radiant/radiant-japanese_language_pack-extension"
     
   def activate
     puts "=> Activated Custom Japanese Language Pack."
